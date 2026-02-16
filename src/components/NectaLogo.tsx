8d070e6be92d0bbe9ae3b50dc5f93e11cb316f76
[dyad] <kelvinjamesmasolwa@gmail.com>
2026-02-16 12:29:28 +0300

    [dyad] Updating NECTA logo image - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/NectaLogo.tsx
+++ src/components/NectaLogo.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 const NectaLogo = ({ className }: { className?: string }) => {
   return (
-    <svg 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
+    <img 
+      src="/images/NECTA.png" 
+      alt="NECTA Logo" 
       className={className}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-    </svg>
+      style={{ objectFit: 'contain' }}
+    />
   );
 };
 
