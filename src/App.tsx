c24a631ad093014c0c69a7cb1bb56759dfa6357b
[dyad] <kelvinjamesmasolwa@gmail.com>
2026-02-17 14:59:39 +0300

    [dyad] Fixed a syntax error in App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    AuthProvider>
+    <AuthProvider>
       <TooltipProvider>
         <Toaster />
         <Sonner />
